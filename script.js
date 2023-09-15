a92a8d28d5fee894527e858955a28ee50cfceb86
Ernest Spiller <e.spiller2002@gmail.com>
2023-09-15 00:32:52 -0700

    Contact visibility functionality top header.

--- script.js
+++ script.js
@@ -10,11 +10,14 @@ window.addEventListener('scroll', function() {
 function toggleHeader() {
     var header = document.getElementById('header');
     var nameElement = header.querySelector('h1');
+    var contactInfo = document.getElementById('contact_info');
 
     if (nameElement.textContent === 'ERNEST SPILLER') {
-        nameElement.textContent = 'New Information';
+        nameElement.textContent = ''; // Hide the name
+        contactInfo.classList.add('active'); // Add 'active' class to show contact info
         setTimeout(function() {
             nameElement.textContent = 'ERNEST SPILLER';
-        }, 3000); // Reverts back to 'Your Name' after 3 seconds
+            contactInfo.classList.remove('active'); // Remove 'active' class after 3 seconds
+        }, 3000);
     }
 }
