53178f2913f92e03ca5651f7df41dbe92dfc63aa
Ernest Spiller <e.spiller2002@gmail.com>
2023-09-18 21:30:42 -0700

    Updated packages and files to work together.

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 // IMPORTS
+import * as THREE from 'three';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -12,6 +13,20 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
 
 // FUNCTIONS
 function toggleHeader() { // Top header hover animation
