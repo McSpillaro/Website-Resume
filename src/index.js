ef2d4c883cb6668be29b91a3484d07547651d7d3
Ernest Spiller <e.spiller2002@gmail.com>
2023-09-19 18:34:06 -0700

    Attempted react.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './Javascript/App';
-import reportWebVitals from './Javascript/reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+import App from './App';
+import reportWebVitals from './components/reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
