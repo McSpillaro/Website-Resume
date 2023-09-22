b0e1b12b3a0efdef1ebba7a321e354c2e5d1618a
Ernest Spiller <e.spiller2002@gmail.com>
2023-09-22 15:55:55 -0700

    Name contact menu
    - Finished functionality feature of the dropdown menu and selection
    - Needs to be bugfixed

--- src/components/nameButton.js
+++ src/components/nameButton.js
@@ -1,29 +1,44 @@
-document.getElementById('top_name').addEventListener('click', function() {
-    window.location.href = 'https://linkedin.com/in/ernestspiller/'; // Redirect to LinkedIn
-});
+// Get all dropdowns from the document
+const dropdowns = document.querySelectorAll('.dropdown');
 
-document.getElementById('top_name').addEventListener('mouseover', function() {
-    document.getElementById('contact_info').style.display = 'block'; // Show the dropdown on hover
-});
+// Loop through all dropdown elements
+dropdowns.forEach(dropdown => {
+    // Get inner elements from each dropdown
+    const select = dropdown.querySelector('.select');
+    const caret = dropdown.querySelector('.caret');
+    const menu = dropdown.querySelector('.menu');
+    const options = dropdown.querySelector('.menu li');
+    const selected = dropdown.querySelector('.selected');
 
-document.getElementById('email').addEventListener('click', function() {
-    copyToClipboard('e.spiller2002@gmail.com', 'Email');
-});
+    // Click evevnt to the select element
+    select.addEventListener('click', () => {
+        // Add the clicked select styles to the select element defined in the topBar.css
+        select.classList.toggle('select-clicked');
+        // Add the rotate styles to the caret element
+        caret.classList.toggle('caret-rotate');
+        // Add the open styles to the contact_info element
+        menu.classList.toggle('.menu-open');
+    });
 
-document.getElementById('phone').addEventListener('click', function() {
-    copyToClipboard('(951) 775-9828', 'Phone');
-});
-
-document.getElementById('linkedin').addEventListener('click', function() {
-    copyToClipboard('linkedin.com/in/ernestspiller/', 'LinkedIn');
-});
-
-function copyToClipboard(text, label) {
-    const el = document.createElement('textarea');
-    el.value = text;
-    document.body.appendChild(el);
-    el.select();
-    document.execCommand('copy');
-    document.body.removeChild(el);
-    alert(`${label} Copied!`);
-}
+    // Loop through all option elements
+    options.forEach(option => {
+        // Add click event to the option element
+        option.addEventListener('click', () => {
+            // Change selected inner text to clicked option inner text
+            selected.innerText = option.innerText;
+            // Add the clicked select styles to the select element
+            select.classList.remove('select-clicked');
+            // Add the rotate styles to the caret element
+            caret.classList.remove('caret-rotate');
+            // Add the open styles to the menu element
+            menu.classList.remove('menu-open');
+            // Remove active class for all option elements
+            options.forEach(option => {
+                option.classList.remove('active');
+            });
+            
+            // Add active class to clicked option element
+            option.classList.add('active');
+        });
+    });
+})
