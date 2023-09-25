a80a5b2c51fa6a102d5661626bbc4282095a672c
Ernest Spiller <e.spiller2002@gmail.com>
2023-09-24 21:58:43 -0700

    Created contact nav bar.

--- react-website-resume/src/App.js
+++ react-website-resume/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+// Imports SVG icons as specified name from icons folder
+import { ReactComponent as PHONE_ICONE } from './icons/phone.svg'
+import { ReactComponent as EMAIL_ICON } from './icons/email.svg'
+import { ReactComponent as LINK_ICON } from './icons/link.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ContactBar> {/* Anything inside here will be fit into the contact-bar section */}
+      <ContactItem icon={<PHONE_ICONE/>} >
+
+      </ContactItem>
+      <ContactItem icon={<EMAIL_ICON/>} >
+
+      </ContactItem>  
+      <ContactItem icon={<LINK_ICON/>} >
+
+      </ContactItem>
+    </ContactBar>
   );
+}
+
+function ContactBar(props) { /* Allowed to take in JSX elements in the App() function */
+  return (
+    <nav className='contactbar'>
+      <ul className='contactbar-nav'> { props.children } </ul>
+    </nav>
+  );
+}
+
+function ContactItem(props) {
+
+  const [copied, setCopied] = useState(false);
+
+  return (
+    <li className='contact-item'>
+      <a href='#' className='icon-button' onClick={() => setCopied(!copied)}>
+        {props.icon}
+      </a>
+
+      {copied && props.children} {/* If copied states is true then it will show the children */}
+    </li>
+  )
 }
 
 export default App;
