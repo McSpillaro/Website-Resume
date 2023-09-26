b7c859be40f88e65474856808c33a32217fc8c38
Ernest Spiller <e.spiller2002@gmail.com>
2023-09-26 09:32:12 -0700

    Update work_experience.ts

--- react-website-resume/src/data/work_experience.ts
+++ react-website-resume/src/data/work_experience.ts
@@ -1,13 +1,13 @@
 const work_experience_data = [
     {
       id: 3,
-      title: "Barista, Sharetea (March 2022 - Present)",
+      title: "Barista, Sharetea",
       content:
         "Cooperate on a team of 10+ baristas to prepare and serve a variety of tea based drinks. Collaborated with team members to inform customers about our products. Train incoming baristas and assisting team members resolve questions on work procedures.",
     },
     {
       id: 4,
-      title: "Instructor, Fearless Tigers (February 2021 - May 2023)",
+      title: "Instructor, Fearless Tigers",
       content:
         "Coordinated group meetings with peer instructors presenting new methods of training. Instructed student exercises, training regimen, sparring, and overall class performance.",
     }
