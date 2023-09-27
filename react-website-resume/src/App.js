d99b749f901a5eacb35c071fdd8a1a0d3f2968ca
Ernest Spiller <e.spiller2002@gmail.com>
2023-09-26 17:05:34 -0700

    Completed contact info section function

--- react-website-resume/src/App.js
+++ react-website-resume/src/App.js
@@ -6,26 +6,25 @@ import './App.css';
 
 // Imports required functions from components
 import Header from './components/Header';
+import Contacts from './components/Contacts';
 import ExperienceAccordion from './components/ExperienceAccordion';
 
-// Imports SVG icons as specified name from icons folder
-import { ReactComponent as PHONE_ICONE } from './icons/phone.svg'
-import { ReactComponent as EMAIL_ICON } from './icons/email.svg'
-import { ReactComponent as LINK_ICON } from './icons/link.svg'
-
 function App() {
-  
+
   return (
-    <div>
+    <>
       <header id='header-title'>
-        <Header></Header>
+        {/* <Header></Header> */}
       </header>
+      <div>
+        <Contacts />
+      </div>
 
       {/* On page, shows expandable tabs with info on experience */}
       <section className="experiences">
         <ExperienceAccordion></ExperienceAccordion>
       </section>
-    </div>
+    </>
   );
 }
 
---
+++ react-website-resume/src/components/Contacts.js
@@ -0,0 +1,107 @@
+import React, { useState, useEffect } from "react";
+import '../styles/Socials.css';
+
+import { ReactComponent as PHONE } from '../icons/call-outline.svg';
+import { ReactComponent as EMAIL } from '../icons/mail-outline.svg';
+
+// Component that creates a mini socials menu on the bottom left of the screen
+function Contacts() {
+    const phoneLabel = 'PHONE';
+    const emailLabel = 'EMAIL';
+    const copiedLabel = 'COPIED!';
+
+    const [content, setContent] = useState(''); // Used for button label text
+    const [labelType, setLabelType] = useState(''); // P => Phone ; E => Email
+    const [isCopied, setIsCopied] = useState(false); // Used for className change
+    const [suffix, setSuffix] = useState("");
+
+    const handleChange = (event) => {
+        setSuffix(event.target.value);
+    };
+
+    const handleClick = () => {
+        setIsCopied(true);
+        // Changes the button label to say 'COPIED!'
+        setContent(copiedLabel);
+        // Handles which text to copy to clipboard based on button clicked
+        if (labelType === 'P') {
+            // Adds the '-copied' class extension on click to change styles
+            setSuffix('-copied');
+            navigator.clipboard.writeText('9517759828');
+        } else if (labelType === 'E') {
+            // Adds the '-copied' class extension on click to change styles
+            setSuffix('-copied');
+            navigator.clipboard.writeText('e.spiller2002@gmail.com');
+        } else {
+            setSuffix('');
+        }
+    }
+
+    return (
+        <div className="container-contacts">
+            <ul>
+                {/* PHONE BUTTON */}
+                <li // NOTE: using the ${} to add to the contact will change its style
+                    // TRY TO CREATE SAME STYLE IN CSS FILE TO BUG FIX
+                    className={`buttons-contacts`}
+                    // Resets the label text to default on enter
+                    onMouseEnter={() => {
+                        setContent(phoneLabel)
+                    }}
+                    // Resets the label text to default on leave
+                    onMouseLeave={() => {
+                        setIsCopied(false); // Undos the copy function to prevent looping
+                        setContent(phoneLabel);
+                    }}
+                    /* Handles copy & paste functionality */
+                    onClick={() => {
+                        setIsCopied(true);
+                        setLabelType('P');
+                        handleClick();
+                    }}
+                >
+                    <span className="icon-contacts">
+
+                    </span>
+                    <span
+                        className="text-contacts"
+                        id="phone"
+                    >
+                        {content}
+                    </span>
+                </li>
+                {/* EMAIL BUTTON */}
+                <li
+                    className={`buttons-contacts${suffix}`}
+                    // Resets the label text to default on enter
+                    onMouseEnter={() => {
+                        setContent(emailLabel)
+                    }}
+                    // Resets the label text to default on leave
+                    onMouseLeave={() => {
+                        setIsCopied(false); // Undos the copy function to prevent looping
+                        setContent(emailLabel);
+                    }}
+                    /* Handles copy & paste functionality */
+                    onClick={() => {
+                        setIsCopied(true);
+                        setLabelType('E');
+                        handleClick();
+                    }}
+                >
+                    <span className="icon-contacts">
+
+                    </span>
+                    <span
+                        className="text-contacts"
+                        id="email"
+                    >
+                        {content}
+                    </span>
+                </li>
+            </ul>
+        </div>
+    );
+}
+
+export default Contacts;
